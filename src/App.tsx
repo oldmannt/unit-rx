cc6c4a4590a415d7226082eb33f1a3aa49c6c072
oldmannt <oldman.mtb@gmail.com>
2019-06-21 00:16:57 -0700

    add input editer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Button from 'antd/lib/button';
 import {DateTimeLabel} from './component/date_time_label'
+import {InputEditer} from './component/input_editer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
-        <DateTimeLabel> </DateTimeLabel>
+        <div className='usnit'>
+          <Button type="primary">Button</Button>
+          <DateTimeLabel> </DateTimeLabel>
+          <InputEditer> </InputEditer>
+        </div>
+        
       </div>
     );
   }
